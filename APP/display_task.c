13a2096b7ac8de2312622e37410de39292640968
Ekko <38444906+Ekkone@users.noreply.github.com>
2018-09-23 16:52:57 +0800

    Update display_task.c

--- APP/display_task.c
+++ APP/display_task.c
@@ -19,7 +19,7 @@ void Display_Task(void const * argument)
   uint8_t string[]="hello world";
   for(;;)
   {
-    //LCD_set_XY(6,6);
+    //LCD_set_XY(6,6);jjj
     //LCD_Display_float(1.11f,10,1);
     //LCD_write_char('C');
     LCD_Write_string(string,0,0);
